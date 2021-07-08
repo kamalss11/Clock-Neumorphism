ff6d1515fc4366317a1c303c266e5b47eec6f52f
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-07-08 12:23:07 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+const deg = 6
+const hr = document.querySelector("#hr")
+const mn = document.querySelector("#mn")
+const sec = document.querySelector("#sec")
+
+setInterval(() => {
+    let day = new Date()
+    let hh = day.getHours() * 30
+    let mm = day.getMinutes() * deg
+    let ss = day.getSeconds() * deg
+    
+    hr.style.transform = `rotateZ(${(hh)+(mm / 12)}deg)`
+    mn.style.transform = `rotateZ(${mm}deg)`
+    sec.style.transform = `rotateZ(${ss}deg)`
+},1000)
